00657eae4ad5828e6f4cc762eec9aad6be8c84e5
Echo <echo199512@hotmail.com>
2023-10-15 00:27:39 -0400

    project1 for fundamental Computer Science course

---
+++ 5001/project/project1/alphabetize.py
@@ -0,0 +1,86 @@
+'''
+    QI NIU
+    16 Sep 2023
+    Lab 2 code file
+    Ask the user for 4 words and print them out in alphabetical order.
+'''
+
+# Input the words
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+word3 = input("Enter the third word: ")
+word4 = input("Enter the fourth word: ")
+
+# Compare and output the result
+print("Words in alphabetical order:")
+# 1. when word1 is place in the first order, follow the pattern to compare the others words
+if word1 <= word2 and word1 <= word3 and word1 <= word4:
+    if word2 <= word3 and word2 <= word4:
+        if word3 <= word4:
+            print(word1, word2, word3, word4)
+        else:
+            print(word1, word2, word4, word3)
+    elif word3 <= word2 and word3 <= word4:
+        if word2 <= word4:
+            print(word1, word3, word2, word4)
+        else:
+            print(word1, word3, word4, word2)
+    elif word4 <= word2 and word4 <= word3:
+        if word2 <= word3:
+            print(word1, word4, word2, word3)
+        else:
+            print(word1, word4, word3, word2)
+
+# 2. when word2 is place in the first order, follow the pattern to compare the others words
+elif word2 <= word1 and word2 <= word3 and word2 <= word4:
+    if word1 <= word3 and word1 <= word4:
+        if word3 <= word4:
+            print(word2, word1, word3, word4)
+        else:
+            print(word2, word1, word4, word3)
+    elif word3 <= word1 and word3 <= word4:
+        if word1 <= word4:
+            print(word2, word3, word1, word4)
+        else:
+            print(word2, word3, word4, word1)
+    elif word4 <= word1 and word4 <= word3:
+        if word1 <= word3:
+            print(word2, word4, word1, word3)
+        else:
+            print(word2, word4, word3, word1)
+
+# 3. when word3 is place in the first order, follow the pattern to compare the others words
+elif word3 <= word1 and word3 <= word2 and word3 <= word4:
+    if word1 <= word4 and word1 <= word4:
+        if word2 <= word4:
+            print(word3, word1, word2, word4)
+        else:
+            print(word3, word1, word4, word2)
+    elif word2 <= word1 and word2 <= word4:
+        if word1 <= word4:
+            print(word3, word2, word1, word4)
+        else:
+            print(word3, word2, word4, word1)
+    elif word4 <= word1 and word4 <= word2:
+        if word2 <= word1:
+            print(word3, word4, word2, word1)
+        else:
+            print(word3, word4, word1, word2)
+
+# 4. when word4 is place in the first order, follow the pattern to compare the others words
+elif word4 <= word1 and word4 <= word2 and word4 <= word3:
+    if word1 <= word3 and word1 <= word2:
+        if word2 <= word3:
+            print(word4, word1, word2, word3)
+        else:
+            print(word4, word1, word3, word2)
+    elif word2 <= word1 and word2 <= word3:
+        if word1 <= word3:
+            print(word4, word2, word1, word3)
+        else:
+            print(word4, word2, word3, word1)
+    elif word3 <= word1 and word3 <= word2:
+        if word1 <= word2:
+            print(word4, word3, word1, word2)
+        else:
+            print(word4, word3, word2, word1)
---
+++ 5001/project/project1/threenumbers.py
@@ -0,0 +1,32 @@
+'''
+    QI NIU
+    16 Sep 2023
+    Lab 2 code file
+    Ask the user for three numbers and print the biggest and smallest and the average of these numbers.
+'''
+
+# Input the nums and convert them into int form in order to calculate
+num1 = int(input("please enter the first number: "))
+num2 = int(input("please enter the second number: "))
+num3 = int(input("please enter the third number: "))
+
+# Find the average of the numbers
+average_num = (num1 + num2 + num3) / 3
+
+# Find the max and min number by compare the numbers and output the results
+if num1 < num2:
+    min_num = num1
+    max_num = num2
+else:
+    min_num = num2
+    max_num = num1
+
+if num3 < min_num:
+    min_num = num3
+else:
+    if num3 > max_num:
+        max_num = num3
+
+# Output the result
+print(
+    f'The biggest number is {max_num}, the smallest number is {min_num}, the average number is {average_num}.')
